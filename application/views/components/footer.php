8507d02efa0e3b034e7111f6f675c1a33a23782d
Andrea Fazio <fazioandrea.f@gmail.com>
2021-07-30 10:45:25 +0200

    Inserito il tutto nelle row

--- application/views/components/footer.php
+++ application/views/components/footer.php
@@ -1,3 +1,5 @@
+</div>
+</main>
             <footer>
                 Footer
             </footer>
--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -1,13 +1,35 @@
 
-    Main Home
- <?php 
+   <h1 class="border-bottom border-dark pt-4 pb-4"  >Corsisti</h1>
+   <!-- <div class="pt-4 pb-4">
+    filtri
+   </div> -->
+   <div class="pt-4 pb-4 container-fluid">
+    <div class="row">
+        <div class="col-4">Nome</div>
+        <div class="col-4">Cognome</div>
+        <div class="col">Etá</div>
+        <div class="col">Genere</div>
+    </div>
+    
+    <?php 
  foreach($arr as $i){
-     echo '<br>';
+     echo '<div class="row">';
      foreach($i as $key=>$value){
-         echo "$key: $value \n";
-     }
- }
- ?>
+         if($key=='nome')
+         echo '<div class="col-4">'.$value.'</div>';
+         if($key=='cognome')
+         echo '<div class="col-4">'.$value.'</div>';
+         if($key=='eta')
+         echo '<div class="col">'.$value.'</div>';
+         if($key=='genere')
+         echo '<div class="col">'.$value.'</div>';
+         
+        }
+       
+        echo '</div>';
+    }
+    ?>
+    </div>
  <a href="main/form">
      Inserimento nuovo  utente
  </a>
